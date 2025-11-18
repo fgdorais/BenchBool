501c215e6c737693424a94b6762ebe3a38035802
François G. Dorais <fgdorais@gmail.com>
2025-11-18 15:42:56 -0500

    randomize

--- BenchBool.lean
+++ BenchBool.lean
@@ -9,18 +9,40 @@ def mmul1 (a : Matrix Bool m n) (x : Vector Bool n) : IO (Vector Bool m) :=
 def mmul2 (a : Matrix Bool m n) (x : Vector Bool n) : IO (Vector Bool m) :=
   pure <| Vector.ofFn fun i => Fin.foldl (n := n) (init := false) fun acc j => strictXor acc <| strictAnd a[i][j] x[j]
 
-/-- not randomly generated 16×100 matrix -/
-def a (size : Nat) : Matrix Bool size size := .ofFn fun _ => .ofFn fun _ => false
-
 /-- not randomly generated 100 vector -/
-def x (size : Nat) : Vector Bool size := .ofFn fun _ => false
+@[noinline]
+def x [RandomGen G] (size : Nat) : Option G → IO (Vector Bool size × Option G)
+  | none => return (.ofFn fun _ => false, none)
+  | some g => do
+      let mut x : Array Bool := #[]
+      let mut g : G := g
+      for _ in [:size] do
+        let p := randBool g
+        x := x.push p.1
+        g := p.2
+      return (.ofFn fun i => x[i]!, g)
 
-def main : IO UInt32 := do
-  let a := a 10000
-  let x := x 10000
+/-- not randomly generated 16×100 matrix -/
+@[noinline]
+def a [RandomGen G] (size : Nat) (g : Option G) : IO (Matrix Bool size size × Option G) := do
+  let mut m : Array (Vector Bool size) := #[]
+  let mut g : Option G := g
+  for _ in [:size] do
+    let p ← x size g
+    m := m.push p.1
+    g := p.2
+  return (.ofFn fun i => m[i]!, g)
 
-  let r1 ← timeit "mmul1:" <| mmul1 a x
-  let r2 ← timeit "mmul2:" <| mmul2 a x
+def main (args : List String) : IO UInt32 := do
+  match args[0]? >>= String.toNat?, args[1]? >>= String.toNat? with
+  | none, _ => return 1
+  | some size, seed =>
+    let g := (mkStdGen ·) <$> seed
+    let (a, g) ← a size g
+    let (x, _) ← x size g
 
-  -- returns zero iff `r1 = r2`
-  return ((Vector.zip r1 r2).any fun (x1, x2) => x1 ^^ x2).toUInt32
+    let r1 ← timeit "mmul1:" <| mmul1 a x
+    let r2 ← timeit "mmul2:" <| mmul2 a x
+
+    -- returns zero iff `r1 = r2`
+    return ((Vector.zip r1 r2).any fun (x1, x2) => x1 ^^ x2).toUInt32
